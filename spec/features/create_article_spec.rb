24066a018227234348b046008e9d4b4fb605dfb3
jerlin joseph <jerlin.urbanecho@gmail.com>
2018-01-26 05:57:19 +0000

    Create Article Successfully

---
+++ spec/features/create_article_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "Creating Articles" do
+    
+    scenario "user creates an article" do
+       
+       visit '/'
+       
+       click_on 'Add an Article'
+       
+       expect(page).to have_content('Create New Article')
+       
+       fill_in 'Title', with: 'First Article'
+       fill_in 'Body', with: 'Lorem ipsum sit dolor amet'
+       
+       click_button 'Create Article'
+       
+       expect(page).to have_content('Article was created successfully')
+       expect(page.current_path).to eq(articles_path)
+        
+    end
+    
+end
