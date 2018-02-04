8483f835d18bfbfd09ccfd370870728bdd79b492
jerlin joseph <jerlin.urbanecho@gmail.com>
2018-02-04 21:57:49 +0000

    Added restrictions to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,25 @@
 class CommentsController < ApplicationController
     
     def create
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.build(comment_params)
-        @comment.user = current_user
+        unless user_signed_in?
+            flash[:alert] = "Please sign in or sign up first"
+            redirect_to new_user_session_path
         
-        if @comment.save
-           flash[:success] = "Comment was added successfully" 
-           
         else
-           flash.now[:error] =  "Comment was not created"
-        end
+            @article = Article.find(params[:article_id])
+            @comment = @article.comments.build(comment_params)
+            @comment.user = current_user
         
-        redirect_to article_path(@article)
-    end
+            if @comment.save
+                flash[:success] = "Comment was added successfully" 
+           
+            else
+                flash.now[:error] =  "Comment was not created"
+            end
+        
+            redirect_to article_path(@article)
+        end
+    end    
     
     private
     def comment_params
